627d11263d0d5f15c32d30265042078f2ccd9838
kulczajr <kulczajr@rose-hulman.edu>
2014-10-27 19:38:34 -0400

    Login is now working mostly

--- main.py
+++ main.py
@@ -25,25 +25,56 @@ jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     autoescape=True)
 
+def employee_key(username):
+    return ndb.Key('Employee', username)
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template("templates/mainpage.html")
         self.response.write(template.render())
 
     def post(self):
-        self.redirect('/home')
+        template = jinja_env.get_template("templates/mainpage.html")
+        self.response.write(template.render())
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template("templates/homepage.html")
         self.response.write(template.render())
 
+class RegisterAction(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get('newusername')
+
+        if (username):
+            employee_query = Employee.query(ancestor=employee_key(username))
+            employees = employee_query.fetch()
+
+            if (not employees):
+                password = self.request.get('newpassword')
+                first_name = self.request.get('fname')
+                last_name = self.request.get('lname')
+
+                if (username and password and first_name and last_name):
+                    new_employee = Employee(parent=employee_key(username), login_name=username, password=password, first_name=first_name, last_name=last_name)
+                    new_employee.put()
+                    self.redirect('/home')
+
 class LoginAction(webapp2.RequestHandler):
     def post(self):
-        self.redirect('/home')
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        if (username and password):
+            employee_query = Employee.query(Employee.login_name == username, Employee.password == password)
+            employees = employee_query.fetch()
+
+            if (employees):
+                self.redirect('/home')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/register', RegisterAction),
     ('/login', LoginAction),
     ('/home', HomePage)
 ], debug=True)
--- models.py
+++ models.py
@@ -4,4 +4,7 @@ from google.appengine.ext import ndb
 class Employee(ndb.Model):
 	""" Information for a user. """
 	login_name = ndb.StringProperty()
- 
+ 	password = ndb.StringProperty()
+ 	first_name = ndb.StringProperty()
+ 	last_name = ndb.StringProperty()
+ 	is_admin = ndb.BooleanProperty(default=False)
