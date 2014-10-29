0bacb23626d13b8bf39e1993e1a943e24b4a24f7
kulczajr <kulczajr@rose-hulman.edu>
2014-10-29 00:14:16 -0400

    Login data storing and session data are now functional

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ import webapp2
 
 import os
 import jinja2
+import logging
 from models import Employee
 from google.appengine.ext import ndb
+from webapp2_extras import sessions
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -28,7 +30,25 @@ jinja_env = jinja2.Environment(
 def employee_key(username):
     return ndb.Key('Employee', username)
 
-class MainPage(webapp2.RequestHandler):
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+}
+
+class BaseHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session()
+
+class MainPage(BaseHandler):
     def get(self):
         template = jinja_env.get_template("templates/mainpage.html")
         self.response.write(template.render())
@@ -37,30 +57,42 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_env.get_template("templates/mainpage.html")
         self.response.write(template.render())
 
-class HomePage(webapp2.RequestHandler):
+class HomePage(BaseHandler):
     def get(self):
+        user_values = {
+            'username': self.session.get('username'),
+            'first_name': self.session.get('first_name'),
+            'last_name': self.session.get('last_name'),
+            'is_admin': self.session.get('is_admin')
+        }
         template = jinja_env.get_template("templates/homepage.html")
-        self.response.write(template.render())
+        self.response.write(template.render(user_values))
 
-class RegisterAction(webapp2.RequestHandler):
+class RegisterAction(BaseHandler):
     def post(self):
         username = self.request.get('newusername')
+        password = self.request.get('newpassword')
+        first_name = self.request.get('fname')
+        last_name = self.request.get('lname')
 
-        if (username):
+        if (username  and password and first_name and last_name):
             employee_query = Employee.query(ancestor=employee_key(username))
             employees = employee_query.fetch()
 
             if (not employees):
-                password = self.request.get('newpassword')
-                first_name = self.request.get('fname')
-                last_name = self.request.get('lname')
-
-                if (username and password and first_name and last_name):
                     new_employee = Employee(parent=employee_key(username), login_name=username, password=password, first_name=first_name, last_name=last_name)
                     new_employee.put()
+                    self.session['username'] = username
+                    self.session['first_name'] = employees[0].first_name
+                    self.session['last_name'] = employees[0].last_name
+                    self.session['is_admin'] = employees[0].is_admin
                     self.redirect('/home')
+            else:
+                self.redirect('/')
+        else:
+            self.redirect('/')
 
-class LoginAction(webapp2.RequestHandler):
+class LoginAction(BaseHandler):
     def post(self):
         username = self.request.get('username')
         password = self.request.get('password')
@@ -70,11 +102,19 @@ class LoginAction(webapp2.RequestHandler):
             employees = employee_query.fetch()
 
             if (employees):
+                self.session['username'] = username
+                self.session['first_name'] = employees[0].first_name
+                self.session['last_name'] = employees[0].last_name
+                self.session['is_admin'] = employees[0].is_admin
                 self.redirect('/home')
+            else:
+                self.redirect('/')
+        else:
+            self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/register', RegisterAction),
     ('/login', LoginAction),
     ('/home', HomePage)
-], debug=True)
+], debug=True, config=config)
--- models.py
+++ models.py
@@ -8,3 +8,11 @@ class Employee(ndb.Model):
  	first_name = ndb.StringProperty()
  	last_name = ndb.StringProperty()
  	is_admin = ndb.BooleanProperty(default=False)
+
+class Timesheet(ndb.Model):
+ 	id = ndb.IntegerProperty()
+ 	worked_by = ndb.KeyProperty(kind=Employee)
+ 	hours_worked = ndb.IntegerProperty(repeated=True)
+ 	time_period = ndb.StringProperty()
+ 	is_submitted = ndb.BooleanProperty()
+ 	is_approved = ndb.BooleanProperty()
